263ef63b134f29de516844413ecba12d08e8ac30
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-16 14:44:59 -0300

    .

--- soma.js
+++ soma.js
@@ -1,23 +1,10 @@
 function gerarTabuada() {
-
-    const numero = Number(document.getElementById("numero").value);
-
-    const resultado = document.getElementById("resultado");
+    let numero = Number(document.getElementById("numero").value);
+    let resultado = document.getElementById("resultado");
 
     resultado.innerHTML = "";
 
-    if (document.getElementById("numero").value === "") {
-        resultado.innerHTML = "<p>Digite um número válido!</p>";
-        return;
-    }
-
     for (let i = 1; i <= 10; i++) {
-        const soma = numero + i;
-
-        resultado.innerHTML += `
-            <div class="linha">
-                ${numero} + ${i} = <strong>${soma}</strong>
-            </div>
-        `;
+        resultado.innerHTML += `${numero} + ${i} = ${numero + i}<br>`;
     }
 }
